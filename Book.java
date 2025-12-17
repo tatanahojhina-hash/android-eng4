301e421040bf0643bbf7acadf8d2d06059b7e330
Tatiana khoykhina <tatanahojhina@gmail.com>
2025-12-17 23:40:34 +0300

    created a class book and information subsections

---
+++ Book.java
@@ -0,0 +1,13 @@
+// Book.java 
+public class Book { 
+    private String title; 
+    private int releaseYear; 
+    private String author; 
+    private int pages; 
+ 
+    public Book(String title, int releaseYear, String author, int pages) { 
+        this.title = title; 
+        this.releaseYear = releaseYear; 
+        this.author = author; 
+        this.pages = pages; 
+    }
